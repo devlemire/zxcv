a3572f257fb93f60853acdcd6f06a11307bc295d
= <webdev.jameslemire@gmail.com>
2017-10-19 12:49:25 -0600

    JL master - Remove unnecessary method.

--- src/components/Question/One/One.js
+++ src/components/Question/One/One.js
@@ -2,20 +2,15 @@ import React, { Component } from 'react';
 import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 
 export default class One extends Component {
-  handleSelect( currentQuestion, i ) {
-    const { select } = this.props;
-    select( currentQuestion, [ i ] );
-  }
-
   render() {
-    const { options, currentQuestion, currentAnswers } = this.props;
+    const { options, currentQuestion, currentAnswers, select } = this.props;
     console.log( options, currentQuestion, currentAnswers, currentAnswers[0] );
 
     return (
       <div className="OneAnswer__container">
         <RadioButtonGroup name={ `radio-group-${ currentQuestion }` } 
                           defaultSelected={ currentAnswers[0] } 
-                          onChange={ (e, value) => this.handleSelect( currentQuestion, value ) }>
+                          onChange={ (e, value) => select( currentQuestion, [ value ] ) }>
         {
           options.map( (option, i) => (
             <RadioButton key={ `radio-${ currentQuestion }-${ i }` }
