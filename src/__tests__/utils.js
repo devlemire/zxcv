b7effcc182d0d6b76e5e3fc5dc105c4e760fa949
= <webdev.jameslemire@gmail.com>
2017-10-20 12:08:34 -0600

    JL master - Add util unit tests skeleton.

---
+++ src/__tests__/utils.js
@@ -0,0 +1,9 @@
+import CIP from '../utils/categoriesInPercent';
+import determineSelected from '../utils/determineSelected';
+import sortCategories from '../utils/sortCategories';
+
+describe("utils/ tests", () => {
+  test("True is true.", () => {
+    expect( true ).toEqual( true );
+  });
+});
