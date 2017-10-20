6b5ee51dd82f170b37bdd68651523ed4d07c1d4e
= <webdev.jameslemire@gmail.com>
2017-10-20 13:12:00 -0600

    JL master - Production URL.

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ let api = {
 if ( process.env.NODE_ENV === 'development' ) {
   api.base = 'http://localhost:3000';
 } else {
-  api.base = 'TBD';
+  api.base = 'http://104.131.99.159:10005';
 }
 
 export default api;
